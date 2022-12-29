84644307846a39f0e8b92f1ac7c560de39d28adc
grafek <jacek.grafender@gmail.com>
2022-12-29 17:58:18 +0100

    front-end layout w/react-icons and prisma post model

---
+++ src/pages/create-post.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Layout } from "../components/Layout/";
+
+const CreatePost: NextPage = () => {
+  return (
+    <Layout>
+      <div>Create Post Page</div>
+    </Layout>
+  );
+};
+
+export default CreatePost;
---
+++ src/pages/my-profile.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Layout } from "../components/Layout/";
+
+const MyProfile: NextPage = () => {
+  return (
+    <Layout>
+      <div>My Profile</div>
+    </Layout>
+  );
+};
+
+export default MyProfile;
