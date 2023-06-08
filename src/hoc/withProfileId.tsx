4dd843fc830aba0346605d80b3100190a4c37c73
grafek <jacek.grafender@gmail.com>
2023-06-08 13:42:18 +0200

    fix:conflicts fix

--- src/hoc/withProfileId.tsx
+++ src/hoc/withProfileId.tsx
@@ -3,7 +3,6 @@ import type { GetStaticProps } from "next";
 import { createServerSideHelpers } from '@trpc/react-query/server';
 import { createContextInner } from "../server/trpc/context";
 import { appRouter } from "../server/trpc/router/_app";
-import { createServerSideHelpers } from '@trpc/react-query/server';
 
 function withProfileId(gsp: GetStaticProps): GetStaticProps {
   return async (context) => {
